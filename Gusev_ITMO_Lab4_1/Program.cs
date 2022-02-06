557fb673edfd02c5b9cfb57c521844900913f2e1
999Newone999 <boris_mail@list.ru>
2022-02-06 17:45:04 +0300

    Добавьте файлы проекта.

---
+++ Gusev_ITMO_Lab4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gusev_ITMO_Lab4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
